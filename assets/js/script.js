df8c697dfb021a93d254ab20849855504b27aeda
miamauro <miiamauro@outlook.com>
2023-02-11 17:27:06 -0600

    ask for user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 // Assignment code here
+//Declare variables in global memory.
 var lowercaseOpt = "abcdefghijklmnopqrstuvwxyz";
 var uppercaseOpt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numberOpt = "1234567890";
@@ -8,6 +9,30 @@ var selectedCharacters = "";
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+//Ask user for the desired password length and which characters they'd like to include.
+//Store these answers for later use.
+function generatePassword() {
+  var passwordLength = prompt(
+    "How many characters would you like your password to be?"
+  );
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Your password must be between 8 and 128 characters.");
+    generatePassword();
+  }
+
+  var lowercaseYes = confirm(
+    "Click OK to confirm the use of lowercase letters or Cancel to exclude lowercase letters."
+  );
+  var uppercaseYes = confirm(
+    "Click OK to confirm the use of uppercase letters or Cancel to exclude uppercase letters."
+  );
+  var numbersYes = confirm(
+    "Click OK to confirm the use of numbers or Cancel to exclude numbers."
+  );
+  var specialcharYes = confirm(
+    "Click OK to confirm the use of special characters or Cancel to exclude special characters."
+  );
+}
 
 // Write password to the #password input
 function writePassword() {
